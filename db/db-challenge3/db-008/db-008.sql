18642da223dc45ad61aee4f7f8e0f87e501becb8
horik <codeschoolway@gmail.com>
2020-06-10 11:58:22 +0900

    問題番号008の修正

--- db/db-challenge3/db-008/db-008.sql
+++ db/db-challenge3/db-008/db-008.sql
@@ -1,18 +1,20 @@
 SELECT
-    user_name,
+    users.user_name,
     chat_rooms.chat_name,
-    join_table.join_date
+    SUBSTRING(join_table.join_date, 1, 10)
 FROM
     join_table
     JOIN
         users
-    ON  (join_table.user_id = users.user_id
-        AND users.is_deleted <> 1
-        )
+    ON
+        join_table.user_id = users.user_id
     JOIN
         chat_rooms
-    ON  (chat_rooms.chat_room_id = join_table.chat_room_id
-        AND chat_rooms.is_deleted <> 1
-        )
+    ON
+        join_table.chat_room_id = chat_rooms.chat_room_id
+WHERE
+    users.is_deleted = 0
+AND chat_rooms.is_deleted = 0
 ORDER BY
     join_table.join_date
+    
