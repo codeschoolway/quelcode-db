8db940ce78c5615c8e06d67fbfed397879065ae9
horik <codeschoolway@gmail.com>
2020-06-10 16:56:18 +0900

    課題011 追加

---
+++ db/db-challenge3/db-011/db-011.sql
@@ -0,0 +1,13 @@
+BEGIN;
+UPDATE
+    tasks
+SET
+    is_done = 1,
+    updated_at = NOW()
+WHERE
+    (created_at >= '2020-04-26 09:00:00' AND created_at <= '2020-04-26 11:30:00')
+AND
+    is_deleted = 0
+AND
+    update_user_id = 1;
+COMMIT;
